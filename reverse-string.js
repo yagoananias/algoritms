a63d7a9ccd2a8b62cfca7bfdeb08da685445709f
Yago Ananias <yago.crazy@hotmail.com>
2021-01-05 15:29:30 -0300

    Create reverse-string.js

---
+++ reverse-string.js
@@ -0,0 +1,14 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  
+  return str;
+}
+
+module.exports = reverse;
