ce6a869c82d0d853319acbc7c9b8ca4cd75e3609
Yago Ananias <yago.crazy@hotmail.com>
2021-02-05 23:12:30 -0300

    Create add-array.js

---
+++ add-array.js
@@ -0,0 +1,7 @@
+Array.prototype.insertFirstPosition = function(value) {
+  for(let i = this.length; i >=0; i--) {
+    this[i] = this[i-1];
+  }
+  this[0] = value;
+ };
+ numbers.insertFirstPosition(-1)
