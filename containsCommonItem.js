2217174628afd7ec82f5ccc392217a8881a089b6
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-07 19:58:06 -0300

    Second Aproach

--- containsCommonItem.js
+++ containsCommonItem.js
@@ -3,21 +3,30 @@
 //const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
 //should return false.
 //-----------
-const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
-//should return true.
+// const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+// //should return true.
 
-// 2 parameters - arrays - no size limit
-// return true or false
+// // 2 parameters - arrays - no size limit
+// // return true or false
 
-function containsCommonItem(arr1, arr2) {
-  for(let i = 0; i < arr1.lenght; i++) {
-    for(let j = 0; j < arr2.lenght; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true
-      }
-    }
-  }
-  return false;
+// function containsCommonItem(arr1, arr2) {
+//   for(let i = 0; i < arr1.lenght; i++) {
+//     for(let j = 0; j < arr2.lenght; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// contanisCommonItem(arr1, arr2)
+
+//Hash tables / objects aproach
+function containsCommonItem2 (arr1, arr2) {
+  //loop through firs array and create an object
+  //where propertie are equal itens in the array
+  //then loop through second array and check if item in
+  //second array exists on created object
 }
 
-contanisCommonItem(arr1, arr2)
