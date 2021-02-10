e74d1d0a425eb90444576e7482c06f9292200cf8
Yago Ananias <yago.crazy@hotmail.com>
2021-02-10 05:10:03 -0300

    Update arrays-ds.js

--- arrays-ds.js
+++ arrays-ds.js
@@ -6,4 +6,7 @@ strings[2]
 //push adds an item at the end of an array (it is a O(1) operation)
 strings.push('e);
              
+//pop is the oposite of push, in fact pop delete the last item in a array. It is a O(1) operation too.
+strings.pop()
+
 console.log(strings);
