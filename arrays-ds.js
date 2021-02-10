28cbfa12fa53fe295fc5474e410b05aa5a54a865
Yago Ananias <yago.crazy@hotmail.com>
2021-02-10 05:07:55 -0300

    Update arrays-ds.js

--- arrays-ds.js
+++ arrays-ds.js
@@ -2,3 +2,8 @@ const string = [ 'a', 'b', 'c', 'd' ];
 //4*4 = 16 bytes of storage
 
 strings[2]
+
+//push adds an item at the end of an array (it is a O(1) operation)
+strings.push('e);
+             
+console.log(strings);
