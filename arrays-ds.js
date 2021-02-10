ac5d9e0775c2913b860381538fc3467011e4cd75
Yago Ananias <yago.crazy@hotmail.com>
2021-02-10 05:13:15 -0300

    Update arrays-ds.js

--- arrays-ds.js
+++ arrays-ds.js
@@ -9,4 +9,7 @@ strings.push('e);
 //pop is the oposite of push, in fact pop delete the last item in a array. It is a O(1) operation too.
 strings.pop()
 
+//unshift adds an item at the BEGINNING of an array. The operation is O(n) because it needs to manipulate every item in an array
+strings.unshift('x')
+
 console.log(strings);
